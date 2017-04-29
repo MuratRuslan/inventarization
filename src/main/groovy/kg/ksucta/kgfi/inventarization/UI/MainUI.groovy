5488cf8ba43a0838ee187479eb823002b5041292
mirbor <bmirbek@gmail.com>
2017-04-29 16:27:23 +0600

    flyway support added

--- src/main/groovy/kg/ksucta/kgfi/inventarization/UI/MainUI.groovy
+++ src/main/groovy/kg/ksucta/kgfi/inventarization/UI/MainUI.groovy
@@ -3,7 +3,9 @@ package kg.ksucta.kgfi.inventarization.UI
 import com.vaadin.annotations.Theme
 import com.vaadin.server.VaadinRequest
 import com.vaadin.spring.annotation.SpringUI
+import com.vaadin.ui.Label
 import com.vaadin.ui.UI
+import com.vaadin.ui.VerticalLayout
 
 /**
  * Created by murat on 4/28/17.
@@ -13,6 +15,8 @@ import com.vaadin.ui.UI
 class MainUI extends UI{
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-
+        VerticalLayout layout = new VerticalLayout()
+        layout.addComponent(new Label("hello"))
+        setContent(layout)
     }
 }
---
+++ src/main/resources/db/migration/V1_1__Initial_version.sql
@@ -0,0 +1,97 @@
+CREATE TABLE "person" (
+  "id" serial NOT NULL,
+  "name" varchar(100) NOT NULL,
+  "lastname" varchar(100) NOT NULL,
+  "login" varchar(100) UNIQUE,
+  "password" varchar(50),
+  CONSTRAINT person_pk PRIMARY KEY ("id")
+) WITH (
+OIDS=FALSE
+);
+
+
+
+CREATE TABLE "Item" (
+  "id" serial NOT NULL,
+  "category" int,
+  "place" bigint NOT NULL,
+  "purchaseDate" DATE,
+  "registrationDate" DATE,
+  "responsiblePerson" bigint,
+  "description" varchar(250),
+  "name" varchar(100) NOT NULL,
+  "inventarNumber" varchar(250) NOT NULL UNIQUE,
+  "cost" money,
+  CONSTRAINT Item_pk PRIMARY KEY ("id")
+) WITH (
+OIDS=FALSE
+);
+
+
+
+CREATE TABLE "category" (
+  "id" serial NOT NULL,
+  "name" varchar(100) NOT NULL UNIQUE,
+  "description" varchar(250),
+  CONSTRAINT category_pk PRIMARY KEY ("id")
+) WITH (
+OIDS=FALSE
+);
+
+
+
+CREATE TABLE "place" (
+  "id" serial NOT NULL,
+  "name" varchar(100) NOT NULL UNIQUE,
+  "description" varchar(250) NOT NULL,
+  CONSTRAINT place_pk PRIMARY KEY ("id")
+) WITH (
+OIDS=FALSE
+);
+
+
+
+CREATE TABLE "personRoles" (
+  "personId" bigint NOT NULL,
+  "role" int NOT NULL
+) WITH (
+OIDS=FALSE
+);
+
+
+
+CREATE TABLE "role" (
+  "id" serial NOT NULL,
+  "name" varchar(100) NOT NULL UNIQUE,
+  "description" varchar(250),
+  CONSTRAINT role_pk PRIMARY KEY ("id")
+) WITH (
+OIDS=FALSE
+);
+
+
+
+CREATE TABLE "image" (
+  "id" serial NOT NULL,
+  "path" varchar,
+  "item_id" bigint,
+  CONSTRAINT image_pk PRIMARY KEY ("id")
+) WITH (
+OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "Item" ADD CONSTRAINT "Item_fk0" FOREIGN KEY ("category") REFERENCES "category"("id");
+ALTER TABLE "Item" ADD CONSTRAINT "Item_fk1" FOREIGN KEY ("place") REFERENCES "place"("id");
+ALTER TABLE "Item" ADD CONSTRAINT "Item_fk2" FOREIGN KEY ("responsiblePerson") REFERENCES "person"("id");
+
+
+
+ALTER TABLE "personRoles" ADD CONSTRAINT "personRoles_fk0" FOREIGN KEY ("personId") REFERENCES "person"("id");
+ALTER TABLE "personRoles" ADD CONSTRAINT "personRoles_fk1" FOREIGN KEY ("role") REFERENCES "role"("id");
+
+
+ALTER TABLE "image" ADD CONSTRAINT "image_fk0" FOREIGN KEY ("item_id") REFERENCES "Item"("id");
+
